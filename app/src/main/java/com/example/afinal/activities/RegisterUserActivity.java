cb380ce0493251f879f8114524c01855e2e1dcb2
“MishelK” <57485490+LevanaSciari@users.noreply.github.com>
2022-08-05 11:10:24 +0300

    Registration activity redesign

--- app/src/main/java/com/example/afinal/activities/RegisterUserActivity.java
+++ app/src/main/java/com/example/afinal/activities/RegisterUserActivity.java
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,12 +24,13 @@ import java.util.ArrayList;
 
 public class RegisterUserActivity extends AppCompatActivity  {
 
-    String[] employeeOptions = {"Internal", "External", "Student"};
-    String[] jobTitles = {"Programmer", "Communication", "Cleaner"};
+    String[] employeeOptions = {"עובד פנימי", "עובד חיצוני", "סטודנט"};
+    String[] jobTitles = {"מפתחת", "מתקשר", "מנקה"};
 
     private Spinner spinnerEmployeeType;
     private Spinner spinnerJobTitle;
-    Button signUpBtn;
+    View layoutEtFirstName, layoutEtLastName, layoutEtMail, layoutEtPhoneNumber, layoutEtEmployeeNum, layoutEtIdNum;
+    View layoutBtnSubmit;
     EditText firstName;
     EditText lastName;
     EditText emailEditText;
@@ -48,18 +50,31 @@ public class RegisterUserActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
         // Finding views
-        signUpBtn = findViewById(R.id.signUpBtn);
-        firstName = (EditText) findViewById(R.id.FirstName);
-        lastName = (EditText) findViewById(R.id.LastName);
-        emailEditText = (EditText) findViewById(R.id.mail);
-        userId = (EditText) findViewById(R.id.user_id);
-        cell_phone = (EditText) findViewById(R.id.cell_phone);
-        uniqueIdEt = (EditText) findViewById(R.id.NumberWork);
+        layoutEtFirstName = findViewById(R.id.layoutEtFirstName);
+        layoutEtLastName = findViewById(R.id.layoutEtLastName);
+        layoutEtMail = findViewById(R.id.layoutEtMail);
+        layoutEtPhoneNumber = findViewById(R.id.layoutEtPhoneNumber);
+        layoutEtEmployeeNum = findViewById(R.id.layoutEtEmployeeNum);
+        layoutEtIdNum = findViewById(R.id.layoutEtIdNum);
+        layoutBtnSubmit = findViewById(R.id.layoutBtnSubmit);
+        firstName = layoutEtFirstName.findViewById(R.id.editText);
+        lastName = layoutEtLastName.findViewById(R.id.editText);
+        emailEditText = layoutEtMail.findViewById(R.id.editText);
+        userId = layoutEtIdNum.findViewById(R.id.editText);
+        cell_phone = layoutEtPhoneNumber.findViewById(R.id.editText);
+        uniqueIdEt = layoutEtEmployeeNum.findViewById(R.id.editText);
         spinnerEmployeeType = findViewById(R.id.spinnerEmployeeType);
         spinnerJobTitle = findViewById(R.id.spinnerJobTitle);
-
+        // Setting texts
+        firstName.setHint("שם פרטי");
+        lastName.setHint("שם משפחה");
+        emailEditText.setHint("מייל");
+        userId.setHint("מספר ת.ז");
+        cell_phone.setHint("מספר טלפון");
+        uniqueIdEt.setHint("מספר עובד");
+        ((TextView) layoutBtnSubmit.findViewById(R.id.textView)).setText("המשך");
         // Setting listeners
-        signUpBtn.setOnClickListener(v -> {
+        layoutBtnSubmit.setOnClickListener(v -> {
             attemptRegistration();
         });
         // Filling spinners with data
